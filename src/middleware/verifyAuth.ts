965d237b4956b7070d7b4c1bbb0af053f76c135d
kvitorr <kvitorrsantos@gmail.com>
2023-01-08 00:58:50 -0300

    Adição do middleware de autenticação

---
+++ src/middleware/verifyAuth.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+
+export function verifyAuth (request: Request, response: Response, next: NextFunction) { // faz a validação e se td der certo, chama a next
+    const authToken = request.headers.authorization
+
+    if(authToken) {
+        const [, token] = authToken.split(' ')
+
+        try {
+            const { sub } = verify(token, '123456789')
+            console.log('Token for user', sub)
+            return next()
+        } catch(error: any) {
+            return response.status(401).json( {message: 'Unauthorized'} )
+        }
+    } 
+
+    return response.status(401).json( {message: 'Unauthorized'} )
+}
