1346e0f2d514bc22c657d925adc087109e466003
kvitorr <kvitorrsantos@gmail.com>
2023-01-07 23:19:24 -0300

    refatoração do método de login

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from 'express'
 import { sign } from 'jsonwebtoken'
+import { UserService } from '../services/UserService'
 
 export class LoginController {
+
+    userService: UserService;
+
+    constructor(userService = new UserService()){
+        this.userService = userService;
+    }
+
     login = async (request: Request, response: Response) => {
-        const tokenData = {
-            name: user.name,
-            email: user.email
+        const {email, password } = request.body
+        
+        try {
+            const token = await this.userService.getToken(email, password)
+            return response.status(200).json({token})
+        } catch(error: any) {
+            return response.status(500).json({message: error.message})
         }
-
-        const tokenKey = '123456789'
-
-        const tokenOptions = {
-            subject: user.id_user,
-            //expiresIn: '4h'
-        }
-        const token = sign(tokenData, tokenKey, tokenOptions)
-
-        return response.status(200).json({token})
     }
 }
